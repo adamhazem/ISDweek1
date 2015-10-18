deef94a0ef1326282f4debd033dbd1037cbf2dad
adamhazem <adamhazem@hotmail.co.uk>
2015-10-18 16:54:08 +0100

    Week one work
    First programme.

---
+++ My First Programme Hello World.py
@@ -0,0 +1,14 @@
+#This programme says hello and ask for my name.
+#len means legth.
+#str means string.
+#int means integer which means number.
+print ("Hello World!")
+print ("What is your name?")
+myName = input ()
+print ("It is good to meet you, " + myName)
+print ("The length of your name is:")
+print (len(myName))
+print ("What is your age?")
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a year.")
+print ("Thank you for using this program " + myName) 
